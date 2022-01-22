986f2c35dfcb8249d1c70a6b5a4a6bc357f39e9e
Theophrastus <tgamboni@uccs.edu>
2022-01-22 13:27:09 -0700

    cant get boost to work on mac

---
+++ recursive_file.cpp
@@ -0,0 +1,10 @@
+#include "boost/filesystem.hpp"
+#include <iostream>
+
+int main () {
+  for ( boost::filesystem::recursive_directory_iterator end, dir("./");
+    dir != end; ++dir ) {
+    // std::cout << *dir << "\n";  // full path
+    std::cout << dir->path().filename() << "\n"; // just last bit
+  }
+}
